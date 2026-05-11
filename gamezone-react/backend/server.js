ec2026ad77381eaedc2918db19a44c69ef4ffd97
Білинський Назар <nazarbilinskij@gmail.com>
2026-05-11 09:42:53 +0300

    use firebase service account from env

--- gamezone-react/backend/server.js
+++ gamezone-react/backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
